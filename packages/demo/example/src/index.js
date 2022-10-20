0ac646a26326b8b345b7d0a5120eea8de6bc6b97
baolilei <baolilei@zhihu.com>
2022-10-20 14:49:29 +0800

    feat: 1

--- packages/demo/example/src/index.js
+++ packages/demo/example/src/index.js
@@ -1,8 +1,9 @@
-// import {a,b} from './index2'
+import {a,b} from './index2'
 
-// const c = b
+// eslint-disable-next-line no-unused-vars
+const c = b
 
-// /*#__PURE__*/ console.log('000');
-// console.log(a);
-// console.log(9999);
+/*#__PURE__*/ console.log('000');
+console.log(a);
+console.log(9999);
 
--- packages/demo/example/src/index2.js
+++ packages/demo/example/src/index2.js
@@ -1,9 +1,9 @@
-// eslint-disable-next-line no-unused-vars
+
 const a = '1'
-// eslint-disable-next-line no-unused-vars
+
 const b = '2'
 
-// export {
-//     a,
-//     b
-// }
+export {
+    a,
+    b
+}
